894d19181b21b8ba89412fcb717b7c8b5011076e
Gary <GaryDeng15@users.noreply.github.com>
2025-01-15 17:24:02 +0800

    安装和卸载封装实现 卸载功能实现，脚本编写，运行还有问题待解决

--- git_install/main.c
+++ git_install/main.c
@@ -1,10 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <Windows.h>
+#include <windows.h>
 #include <stdbool.h>
 
 #define REG_SHELL_DIRECTORY			"*\\Shell\\"
-#define REG_BACKGROUND_DIRECTORY	"Directory\\Background\\Shell\\"
+#define REG_BACKGROUND_DIRECTORY	"Directory\\Background\\shell\\"
 #define REG_DESKTOP_BACKGROUND		"DesktopBackground\\shell\\"
 
 typedef struct {
@@ -15,7 +15,7 @@ typedef struct {
 
 typedef struct {
 	HKEY key;
-	char* path[MAX_PATH];
+	char path[MAX_PATH];
 	int item_count;
 	RegItem items[1024];
 }RegInfo;
@@ -24,9 +24,12 @@ void init_reg_info(RegInfo* info);
 
 bool register_info(RegInfo* info);
 
-bool delete_register_info(RegInfo* info);
+bool delete_register_info(HKEY key, const char* key_name);
+
+bool delete_register_key(HKEY key, const char* key_name, const char *sub_key_name);
 
 int main(int argc, char* argv[]) {
+
 	const char *commit_type = argv[1];
 	const char* path_exe = argv[2];
 	const char* path_icon = argv[3];
@@ -34,14 +37,14 @@ int main(int argc, char* argv[]) {
 
 	char temp_reg_key[MAX_PATH] = { 0 };
 	char temp_sub_reg_key[MAX_PATH] = { 0 };
-	strcpy(temp_reg_key, REG_SHELL_DIRECTORY);
+	strcpy(temp_reg_key, REG_BACKGROUND_DIRECTORY);
 	strcat(temp_reg_key, name);
 
-	strcpy(temp_sub_reg_key, REG_SHELL_DIRECTORY);
+	strcpy(temp_sub_reg_key, REG_BACKGROUND_DIRECTORY);
 	strcat(temp_sub_reg_key, name);
-	strcat(temp_sub_reg_key, "\\Command");
+	strcat(temp_sub_reg_key, "\\command");
 
-	if (strcmp(commit_type, "1")) { // 安装
+	if (strcmp(commit_type, "1") == 0) { // 安装
 		RegInfo reg_info;
 		init_reg_info(&reg_info);
 		strcpy(reg_info.path, temp_reg_key);
@@ -75,8 +78,10 @@ int main(int argc, char* argv[]) {
 			return 1;
 		}
 	}
-	else if (strcmp(commit_type, "2")) { // 卸载
-		
+	else if (strcmp(commit_type, "2") == 0) { // 卸载
+		if (delete_register_info(HKEY_CLASSES_ROOT, temp_reg_key)) {
+			delete_register_key(HKEY_CLASSES_ROOT, REG_BACKGROUND_DIRECTORY, name);
+		}
 	}
 
 	system("pause");
@@ -86,26 +91,15 @@ int main(int argc, char* argv[]) {
 void init_reg_info(RegInfo* info)
 {
 	info->item_count = 0;
-	memset(info->path, 0, sizeof(info));
-	memset(info->items, 0, sizeof(info));
+	memset(info->path, 0, sizeof(info->path));
+	memset(info->items, 0, sizeof(info->items));
 }
 
 bool register_info(RegInfo* info)
 {
 	HKEY result;
 	long result_value = -1;
-	if (result_value = RegCreateKeyEx(
-		info->key,
-		info->path,
-		0,
-		NULL,
-		REG_OPTION_NON_VOLATILE,
-		KEY_CREATE_SUB_KEY | KEY_ALL_ACCESS,
-		NULL,
-		&result,
-		NULL
-		) == ERROR_SUCCESS) {
-
+	if (result_value = RegCreateKeyEx(info->key, info->path, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_CREATE_SUB_KEY | KEY_ALL_ACCESS, NULL, &result, NULL) == ERROR_SUCCESS) {
 		for (int i = 0; i < info->item_count; i++) {
 			char* temp_name = info->items[i].name[0] == '\0' ? NULL : info->items[i].name;
 			if (result_value = RegSetValueEx(
@@ -124,12 +118,65 @@ bool register_info(RegInfo* info)
 		return true;
 	}
 	else {
-		printf("\n error code RegSetValueEx = %i \n", result_value);
+		printf("\n error code RegCreateKeyEx = %i \n", result_value);
 	}
 	return false;
 }
 
-bool delete_register_info(RegInfo* info)
+bool delete_register_info(HKEY key, const char *key_name)
 {
+	HKEY result;
+	long result_value = -1;
+	DWORD count;
+	int error_count = 0;
+	if ((result_value = RegOpenKeyEx(result, key_name, 0, KEY_READ, &result)) == ERROR_SUCCESS) {
+		if ((result_value = RegQueryInfoKey(result, NULL, NULL, NULL, &count, NULL, NULL, NULL, NULL, NULL, NULL, NULL)) == ERROR_SUCCESS) {
+			char sub_key_name[MAX_PATH] = { 0 };
+			for (DWORD i = 0; i < count; i++) {
+				if ((result_value = RegEnumKey(result, i, sub_key_name, MAX_PATH)) == ERROR_SUCCESS) {
+					if ((result_value = RegDeleteKey(result, sub_key_name)) != ERROR_SUCCESS) {
+						error_count++;
+						printf("\n error %s 该键存在子项，无法删除 \n", sub_key_name);
+					}
+				}
+				else {
+					error_count++;
+					printf("\n error \n");
+				}
+			}
+		}
+		else {
+			error_count++;
+			printf("\n error \n");
+		}
+		RegCloseKey(result);
+
+	}
+	else {
+		error_count++;
+		printf("\n error %s 无效 或者 需要管理员权限 \n", key_name);
+	}
+
+	if(!error_count)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool delete_register_key(HKEY key, const char* key_name, const char* sub_key_name)
+{
+	HKEY result;
+	long value = -1;
+	if ((value = RegOpenKeyEx(key, key_name, 0, KEY_READ, &result)) == ERROR_SUCCESS)
+	{
+		if (value = RegDeleteKey(result, sub_key_name) != ERROR_SUCCESS)
+		{
+			return true;
+		}
+	}
+
 	return false;
 }
